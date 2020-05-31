c7f9e0427936b0889c15fc019acc703b3fc1e6d0
Cristhofer <cristhofer.dev@gmail.com>
2020-05-31 14:34:48 -0500

    course backend: databases

--- cf_backend/server.js
+++ cf_backend/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
+const sqlite = require('sqlite3');
+const Sequelize = require('sequelize');
+
 const app = express();
+const sequelize = new Sequelize('cfbackend', null, null, {
+   dialect: 'sqlite',
+   storage: './db/cfbackend'
+});
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static('assets', {
@@ -11,6 +19,7 @@ app.use(cookieSession({
    name: 'session',
    keys: ['5j3dsje438dsd', '303jr95jdj848wbeiso']
 }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(request, response) {
    response.render('index');
@@ -26,6 +35,10 @@ app.get('/visits', function(request, response) {
    request.session.visits = request.session.visits || 0;
    request.session.visits = request.session.visits + 1;
    response.send(`${request.session.visits} visita(s)`);
+});
+
+app.post('/pendientes', function(request, response) {
+   response.send('Inserción Finalizada');
 });
 
 app.listen(3000);
