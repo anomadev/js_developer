e794c3bb3b0c96b93f48d7fb197a6e87beb3a233
Cristhofer Ordaz <cristhofer.dev@gmail.com>
2020-06-19 02:23:31 -0500

    spa with nuxt

---
+++ nuxt_spa/pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <header>
+      <h1 class="title">Álbums de la página</h1>
+    </header>
+
+    <div class="columns is-multiline">
+      <AlbumCard v-for="album in albums" :album="album" :key="album.id"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import env from '../config/env'
+import AlbumCard from "../components/AlbumCard";
+
+export default {
+  name: 'IndexPage',
+  components: {AlbumCard},
+
+  data() {
+    return {
+      albums: []
+    }
+  },
+
+  created: async function() {
+    let response = await axios.get(`${env.endpoint}/albums`)
+    this.albums = response.data
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
