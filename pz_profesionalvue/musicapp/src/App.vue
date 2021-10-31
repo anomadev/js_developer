de68c2baab89a2c58e2b43463cc184f53e791bea
anomadev <cristhofer.dev@gmail.com>
2021-10-31 11:07:21 -0600

    Platzi: Curso de Webpack

---
+++ pz_profesionalvue/musicapp/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: "Hola Vue!"
+    }
+  }
+}
+</script>
+
+<style>
+</style>
