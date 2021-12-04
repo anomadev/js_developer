26e66ed7263926b698dbdb0b8189122428edd202
anomadev <cristhofer.dev@gmail.com>
2021-12-03 19:24:46 -0600

    Platzi: Backend con Node - API REST

---
+++ pz_nodeapi/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const routerApi = require('./routes');
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`API Node listening at http://localhost:${port}`);
+});
